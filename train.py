2d56dd14e2c62464970925ed2bbbaf1c8b04c4ee
anshkumar <anshgravity@gmail.com>
2021-06-05 18:53:50 +0000

    internal changes

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ flags.DEFINE_float('momentum', 0.9,
                    'momentum')
 flags.DEFINE_float('weight_decay', 5 * 1e-4,
                    'weight_decay')
-flags.DEFINE_float('print_interval', 1,
+flags.DEFINE_float('print_interval', 100,
                    'number of iteration between printing loss')
 flags.DEFINE_float('save_interval', 1000,
                    'number of iteration between saving model(checkpoint)')
